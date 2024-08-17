9dbb922a1e115194a5340ec88971d0e230f8ec18
brunojose1977 <brunojose1977@yahoo.com.br>
2024-08-17 16:22:56 -0300

    Diversas mudanças no código para utilização de variáveis de ambiente na configuração da conexção em ambiente de conteiner.

--- database/db.go
+++ database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
@@ -14,7 +15,14 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+
+	stringDeConexao := "host=" + os.Getenv("POSTGRES_HOST") +
+		" user=" + os.Getenv("POSTGRES_USER") +
+		" password=" + os.Getenv("POSTGRES_PASSWORD") +
+		" dbname=" + os.Getenv("POSTGRES_DBNAME") +
+		" Port=" + os.Getenv("POSTGRES_PORT") +
+		" sslmode=disable"
+
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
